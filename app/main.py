39aeba7b8eb15ba7b9bcaf94e748999da1d40911
Alex <yarimov@ya.ru>
2023-11-01 16:11:13 +0300

    init

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(
+    title="FastApi"
+)
+
+
+@app.get("/")
+# Параметры пути
+def get_hello():
+    return "Hello world!"
+
+#
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
